f15d74408bc1400c531c2accac4ffc2266a46c93
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-08-27 10:35:37 +0600

    menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,6 @@
 import { headerLogo } from "../assets/images/index";
+import { hamburger } from "../assets/icons/index";
+import { navLinks } from "../constants/index";
 
 const Nav = () => {
   return (
@@ -8,10 +10,22 @@ const Nav = () => {
           <img src={headerLogo} width={130} height={29} alt="Logo" />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          <li>
-            <a href=""></a>
-          </li>
+          {navLinks.map((item) => (
+            <>
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-monts errat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            </>
+          ))}
         </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger" height={25} width={25} />
+        </div>
       </nav>
     </header>
   );

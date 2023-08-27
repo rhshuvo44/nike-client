35420ac34963ca455c70be08d9c1d56a70317774
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-08-27 13:10:21 +0600

    about us

--- src/sections/SupperQuality.jsx
+++ src/sections/SupperQuality.jsx
@@ -1,5 +1,34 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SupperQuality = () => {
-  return <div>SupperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize lg:max-w-lg font-bold">
+          We Provide you <span className="text-coral-red">Super</span>{" "}
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience ,providing you with unmatched
+          quality,innovation,and a touch of elegance
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img src={shoe8} alt="" width={570} height={522} className="object-contain"/>
+      </div>
+    </section>
+  );
 };
 
 export default SupperQuality;

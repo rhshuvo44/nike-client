db5fff54474bc8527e4b9e132d37d45a67ce9f6b
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-08-27 15:59:53 +0600

    footer

--- src/constants/index.js
+++ src/constants/index.js
@@ -134,7 +134,7 @@ export const footerLinks = [
     title: "Get in touch",
     links: [
       { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-      { name: "+92554862354", link: "tel:+92554862354" },
+      { name: "+192554862354", link: "tel:+192554862354" },
     ],
   },
 ];
